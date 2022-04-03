a3c48bfee4902c4c8ba9bafb08d8390ba3f5dae9
akhilesh <akhileshseechurn@gmail.com>
2022-04-03 16:44:49 +0400

    [8 kyu] 33. Is it a palindrome

---
+++ Python/8 kyu/33. Is it a palindrome/tests.py
@@ -0,0 +1,9 @@
+@test.describe('sample tests')
+def sample_tests():
+    test.assert_equals(is_palindrome('a'), True)
+    test.assert_equals(is_palindrome('aba'), True)
+    test.assert_equals(is_palindrome('Abba'), True)
+    test.assert_equals(is_palindrome('malam'), True)
+    test.assert_equals(is_palindrome('walter'), False)
+    test.assert_equals(is_palindrome('kodok'), True)
+    test.assert_equals(is_palindrome('Kasue'), False)
